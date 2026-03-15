8ee1fd65deb27ca0b50748aa6a4b043524ade0ed
zyexro <zyexro@proton.me>
2026-03-15 20:30:39 +0100

    include hymo kok
    Signed-off-by: zyexro <zyexro@proton.me>

---
+++ ksu/ahymo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# hdjsjfjjwufbeizihfjejzf
+
+export maindir="$(pwd)"
+export outside="${maindir}/.."
+source "${outside}/$1env"
+
+curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/refs/heads/master/kernel/setup.sh" | bash -s master
+git add . && git commit -am "drivers: KernelSU"
+KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
+KSU_ver=$(($KSU_git_ver + 30000))
+
+patchesdir="$outside/ksu/patches/$(echo $kernel_ver | cut -d. -f1,2)"
+suspatchesdir="$outside/ksu/hymo_patches/$(echo $kernel_ver | cut -d. -f1,2)"
+
+if [[ -d "$patchesdir" ]]; then
+  for patch_file in "$patchesdir"/*.patch ; do
+    git am "$patch_file"
+  done
+else
+  echo "patching ksu failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
+
+if [[ -d "$suspatchesdir" ]]; then
+  for patch_file in "$suspatchesdir"/*.patch ; do
+    git am "$patch_file"
+  done
+else
+  echo "patching ksu susfs failed, the kernel version you want to patch doesnt have patches here yet"
+  exit 1
+fi
+
+sed -i "s/\(CONFIG_LOCALVERSION=\)\(.*\)/\1\"-${kernel_name}-ks${KSU_ver}hymo\"/" "${defconfig_file}"
+
+echo "$(grep 'CONFIG_LOCALVERSION=' ${defconfig_file})"
+
+echo -e " \nincludes backslashxx's KernelSU fork, ver ${KSU_ver}" >> banner_append
+echo -e " \nincludes HymoFS" >> banner_append
+
